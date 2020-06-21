502c5584d92d49ae97e467748e0f262c4e24f25b
Jeaninne <jeaninnel@hotmail.com>
2020-06-21 16:52:43 -0400

    setup db Schema and seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,10 @@
-### Schema
+CREATE DATABASE eat_da_burger_db;
+USE eat_da_burger_db;
 
-CREATE DATABASE cat_db;
-USE cat_db;
-
-CREATE TABLE cats
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	eaten BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
